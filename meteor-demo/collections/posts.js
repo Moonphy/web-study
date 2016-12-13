b6e0cdef4c05d45da946cbde3c9d24f375ba120a
Moonphy <337465068@qq.com>
2016-12-13 17:40:45 +0800

    剔除旧项目

--- meteor-demo/collections/posts.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Administrator on 2015/12/19.
- */
-Posts = new Mongo.Collection('posts');
--- meteor/meteor-demo/client/main.js
+++
@@ -1,3 +0,0 @@
-/*Meteor.subscribe('allposts');*/
-
-/*Meteor.subscribe('posts', 'bob-smith');   //订阅author为bob-smith的数据*/
